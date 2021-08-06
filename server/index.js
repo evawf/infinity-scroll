8c152e9f3392e4e3917e0017c7bf00b5cbfdf6dd
eva.wang <eva.wang81@gmail.com>
2021-08-06 09:50:11 +0800

    clean up code

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ if (process.env.Node_ENV !== "production") {
 }
 
 const Unsplash_apiKey = process.env.UNSPLASH_API_KEY;
-const count = 10;
+const count = 5;
 
 const path = require('path');
 const express = require('express');
